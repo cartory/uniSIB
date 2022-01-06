5470cc37e323f6d34ad31e3a855878b8aeddd0ce
cartory <xXcartoryXx@gmail.com>
2022-01-06 17:40:53 -0400

    update databaseSeeder > seed

--- server/seeders/databaseSeeder.js
+++ server/seeders/databaseSeeder.js
@@ -1,14 +1,16 @@
-const autor = require("./autorSeeder");
-const genero = require("./generoSeeder");
-const estudiante = require("./estudianteSeeder");
+const autor = require("./autorSeeder")
+const genero = require("./generoSeeder")
+const estudiante = require("./estudianteSeeder")
 
 const seed = async () => {
-    // await autor.seed(10);
-    // await genero.seed();
-    // await estudiante.seed(10);
+	await autor.seed(10)
+	await genero.seed()
+	await estudiante.seed(10)
 }
 
-
-seed().then(() => {
-    console.log(`\x1b[32mDB DATABASE SEEDED!!\x1b[0m`);
-}).catch(e => console.error(e));
+seed()
+	.then(() => {
+		console.log(`\x1b[32mDB DATABASE SEEDED!!\x1b[0m`)
+	})
+	.catch((e) => console.error(e))
+    .finally(() => process.exit(0))
